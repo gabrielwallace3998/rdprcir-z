2ceec6c18fea7b8ce8acbfcd985c38ecd5355d90
Gabriel <gabriel.wallace.3998@outlook.com>
2026-04-24 19:18:56 -0300

    feat: adicionando speed insights e analytics da vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Orbitron, JetBrains_Mono, Michroma } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const orbitron = Orbitron({
@@ -38,6 +40,8 @@ export default function RootLayout({
         <div className="noise" />
         <div className="vignette" />
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
